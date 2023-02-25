68b8b0188fc02428e72b56670a9a038f9556bc15
Ghulam <gm2025392@gmail.com>
2023-02-26 00:47:29 +0500

    Add some folder to deploy

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const serverless = require('serverless-http')
 const path = require('path')
 const app = express()
+const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const mergedPdf = require('./merge.js')
-app.use('/static', express.static('public'))
+router.use('/static', express.static('public'))
 const port = 3000
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates/index.html'))
 })
 
-app.post('/merge', upload.array('pdfs', 2), async function (req, res, next) {
+router.post('/merge', upload.array('pdfs', 2), async function (req, res, next) {
 if (req.files.length === 2) {
 
     let d = await mergedPdf(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
@@ -26,8 +28,6 @@ if (req.files.length === 2) {
       
 })
 
+app.use('/.netlify/functions/api', router)
 
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on localhost http://localhost:3000')
-})
+module.exports.handler = serverless(app)
